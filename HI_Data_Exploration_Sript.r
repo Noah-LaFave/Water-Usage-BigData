ffcd1ea66143f6ae1b4bee7c53980dc82433b806
Chloe <chloegwyneth4@gmail.com>
2021-02-28 18:59:06 -0800

    Fixed Some things. Plots work now

--- HI_Data_Exploration_Sript.r
+++ HI_Data_Exploration_Sript.r
@@ -17,35 +17,35 @@ HI_usgs2010_data <- usgs2010_data[c(547:551),] #2010
 HI_usgs2015_data <- usgs2015_data[c(548:552),] #2015
 
 #Population vs Total Water Use (in each county)
-
+par(mfrow=c(3,2))
 #1990 
 HI_lm_1990 <- lm(HI_usgs1990_data$`to-total` ~ HI_usgs1990_data$`po-total`)
-plot(HI_usgs1990_data$`po-total`,HI_usgs1990_data$`to-cuse`, xlab = 
+plot(HI_usgs1990_data$`po-total`,HI_usgs1990_data$`to-cuse`, main="1990", xlab = 
        "population", ylab = "water usage total")
 
 #1995
 HI_lm_1995 <- lm(HI_usgs1995_data$`TO-WTotl` ~ HI_usgs1995_data$TotalPop)
-plot(HI_usgs1995_data$TotalPop,HI_usgs1995_data$`TO-CUTot`, xlab = 
+plot(HI_usgs1995_data$TotalPop,HI_usgs1995_data$`TO-CUTot`, main="1995", xlab = 
        "population", ylab = "water withdrawal total")
 
 #2000
 HI_lm_2000 <- lm(HI_usgs2000_data$`TO-WTotl` ~ HI_usgs2000_data$`TP-TotPop`)
-plot(HI_usgs2000_data$`po-total`,HI_usgs2000_data$`to-total`, xlab = 
+plot(HI_usgs2000_data$`TP-TotPop`,HI_usgs2000_data$`TO-WTotl`,main="2000", xlab = 
        "population", ylab = "water withdrawal usage total")
 
 #2005
 HI_lm_2005 <- lm(HI_usgs2005_data$`TO-WTotl` ~ HI_usgs2005_data$`TP-TotPop`)
-plot(HI_usgs2005_data$`po-total`,HI_usgs2005_data$`to-total`, xlab = 
+plot(HI_usgs2005_data$`TP-TotPop`,HI_usgs2005_data$`TO-WTotl`,main="2005", xlab = 
        "population", ylab = "water withdrawal usage total")
 
 #2010
 HI_lm_2010 <- lm(HI_usgs2010_data$`TO-WTotl` ~ HI_usgs2010_data$`TP-TotPop`)
-plot(HI_usgs2010_data$`po-total`,HI_usgs2010_data$`to-total`, xlab = 
+plot(HI_usgs2010_data$`TP-TotPop`,HI_usgs2010_data$`TO-WTotl`, main="2010",xlab = 
        "population", ylab = "water usage total")
 
 #2015
 HI_lm_2015 <- lm(HI_usgs2015_data$X.136 ~ HI_usgs2015_data$X.5)
-plot(HI_usgs2015_data$X.5,HI_usgs2015_data$X.136, xlab = 
+plot(HI_usgs2015_data$X.5,HI_usgs2015_data$X.136, main="2015",xlab = 
        "population", ylab = "water usage total")
 
 
