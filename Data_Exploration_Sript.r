91e1c988483f4e9b57785bc3735f9e3a1c007d46
Chloe <chloegwyneth4@gmail.com>
2021-02-28 17:17:11 -0800

    Load in data script

---
+++ Data_Exploration_Sript.r
@@ -0,0 +1,9 @@
+library(readxl)
+
+#Read in the data from USGS
+usgs1990_data <- read_excel('./Data goes here/us90co.xls')
+usgs1995_data <- read_excel('./Data goes here/usco1995.xlsx')
+usgs2000_data <- read_excel('./Data goes here/usco2000.xls')
+usgs2005_data <- read_excel('./Data goes here/usco2005.xls')
+usgs2010_data <- read_excel('./Data goes here/usco2010.xlsx')
+usgs2015_data <- read.csv('./Data goes here/usco2015v2.0.csv')
